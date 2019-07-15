15c502a2ec45a8ab28675278aade86ceea89c452
soyoungjeong <hana46798@gmail.com>
2019-07-16 00:09:04 +0900

    TwoArraysSum modified

---
+++ src/practice/TwoArraysSum.java
@@ -0,0 +1,70 @@
+package practice;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+//2143번
+public class TwoArraysSum {
+    static ArrayList<Integer> sum(int[] arr, ArrayList<Integer> list){
+        for(int i=0; i<arr.length; i++){
+            int sum = 0;
+            for(int j=i; j<arr.length; j++){
+                sum += arr[j];
+                list.add(sum);
+            }
+        }
+        return list;
+    }
+
+    static int countExist(ArrayList<Integer> list, int n){
+        int cnt = 0;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i)== n){
+                cnt += 1;
+            }
+        }
+        return cnt;
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for(int i=0; i<m; i++){
+            b[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> first = new ArrayList<>();
+        ArrayList<Integer> second = new ArrayList<>();
+
+        first = sum(a, first);
+        second = sum(b, second);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int x : second){
+            if(map.containsKey(x)){
+                int temp = map.get(x);
+                map.put(x, temp+1);
+            }else {
+                map.put(x, 1);
+            }
+
+        }
+        long ans = 0;
+        for(int num : first){
+            if(map.containsKey(t-num)){
+                ans += map.get(t-num);
+            }
+        }
+        System.out.println(ans);
+    }
+}
